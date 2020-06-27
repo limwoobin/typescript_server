9d778556b200c159ca36c8f76e715822fc96ed2b
drogba <drogba02@naver.com>
2020-06-27 14:37:45 +0900

    typescript 세팅#5

--- dist/tsTest.js
+++ dist/tsTest.js
@@ -1,2 +1,6 @@
+"use strict";
 const a = 'asd';
+const b = 50;
+console.log(a);
+console.log(b);
 //# sourceMappingURL=tsTest.js.map
--- src/tsTest.ts
+++ src/tsTest.ts
@@ -1,2 +1,6 @@
 const a:string = 'asd';
+const b:number = 50;
 
+
+console.log(a);
+console.log(b);
