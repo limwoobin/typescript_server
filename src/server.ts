24cf2f0fbdef941f6529ecf47b4d9fe5106aff74
drogba <drogba02@naver.com>
2020-07-04 20:20:09 +0900

    server convert to ts#2

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const history = require('connect-history-api-fallback');
-const bodyParser = require('body-parser');
-const router = require('./routes/router');
+import history from 'connect-history-api-fallback';
+import bodyParser from 'body-parser';
+import router from './routes/router';
 const setting = require('./routes/setting');
 const logger = require('./config/winston');
 const config = require('./config/config.json');
-const session = require('express-session');
-const redis = require('redis');
-// const redisStore = require('connect-redis')(session);
+import session from 'express-session';
+import redis from 'redis';
+// import redisStore from 'connect-redis'(session);
 // const client = redis.createClient();
-const passport = require('passport')
-const compression = require('compression');
+import passport from 'passport';
+import compression from 'compression';
 
 // app.use(session({
 //     store: new redisStore({
@@ -31,32 +31,30 @@ const compression = require('compression');
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(compression());
 
-app.use(compression()); // gzip 압축 사용
-
-app.all('/*' , (req , res , next) => {
+app.all('/*' , (req: express.Request , res: express.Response , next: express.NextFunction) => {
     res.header('Access-Control-Allow-Origin','*');
     res.header('Access-Control-Allow-Headers' , 'X-Requested-With');
     next();
-}); // cors 허용
+});
 
 app.use(setting);
-// app.use(history()); // client와 연결
+// app.use(history());
 // app.use('/' , express.static(__dirname + "/../../typescript_web/build"));
-app.get('/' , (req , res) => {
+app.get('/' , (req: express.Request , res: express.Response) => {
     return res.json('drogbalog api server')
 });
-// 훅스버전
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/dr' , router);
-app.use((err , req , res , next) => {
+app.use((err: express.ErrorRequestHandler , req: express.Request , res: express.Response , next: express.NextFunction) => {
     logger.info(err);
     return res.status(500);
 });
 
-const port = process.env.PORT || 4000;
+const port: (string | number) = process.env.PORT || 4000;
 app.listen(port , () => {
     logger.info(`Listening on port ${port}`);
-});
+})
---
+++ src/server2.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const history = require('connect-history-api-fallback');
+const bodyParser = require('body-parser');
+import router from './routes/router';
+// const router = require('./routes/router');
+const setting = require('./routes/setting');
+const logger = require('./config/winston');
+const config = require('./config/config.json');
+const session = require('express-session');
+const redis = require('redis');
+// const redisStore = require('connect-redis')(session);
+// const client = redis.createClient();
+const passport = require('passport')
+const compression = require('compression');
+
+// app.use(session({
+//     store: new redisStore({
+//         host: config.redis.host,
+//         port: config.redis.port,
+//         client: client,
+//         ttl:200
+//     }),
+//     key: config.session.key,
+//     secret: config.session.secret,
+//     cookie: {
+//         maxAge: 1000 * 60 * 60
+//     },
+//     saveUninitialized: false,
+//     resave: false
+// }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(compression()); // gzip 압축 사용
+
+app.all('/*' , (req , res , next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers' , 'X-Requested-With');
+    next();
+}); // cors 허용
+
+app.use(setting);
+// app.use(history()); // client와 연결
+// app.use('/' , express.static(__dirname + "/../../typescript_web/build"));
+app.get('/' , (req , res) => {
+    return res.json('drogbalog api server')
+});
+// 훅스버전
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/dr' , router);
+app.use((err , req , res , next) => {
+    logger.info(err);
+    return res.status(500);
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port , () => {
+    logger.info(`Listening on port ${port}`);
+});
