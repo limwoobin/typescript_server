5d20f25179f24cb1e018206b1a2766510c2366d0
drogba <drogba02@naver.com>
2020-07-08 20:30:27 +0900

    Response 타입 수정

--- src/core/code/ResponseType.ts
+++ src/core/code/ResponseType.ts
@@ -1,8 +1,20 @@
-interface ResponseType {
-    code: number,
-    message: string,
-    data: object;
-    error: any;
+type ResponseConfig<T> = { code: HttpStatusCode, message: string; data: T; error?: any };
+
+enum HttpStatusCode {
+    OK = 200,
+    NOT_FOUND = 404,
 }
 
-export default ResponseType;
+export default class Response<T> {
+    code: HttpStatusCode = HttpStatusCode.OK;
+    message: string;
+    data: T;
+    error: any;
+    
+    Response(config:ResponseConfig<T>) {
+        this.code = config.code;
+        this.message = config.message;
+        this.data = config.data;
+        this.error = config.error;
+    }
+}
--- src/routes/category/CategoryRoute.ts
+++ src/routes/category/CategoryRoute.ts
@@ -2,22 +2,20 @@ import * as express from 'express';
 const router = express.Router();
 import CategoryService from './CategoryService';
 import HttpStatus from 'http-status';
-import ResponseType from '../../core/code/ResponseType';
+import Response from '../../core/code/ResponseType';
 import CategoryTypeCode from '../../core/code/CategoryTypeCode';
 const logger = require('../../config/winston');
 
 router.get('/list' , async (req: express.Request , res: express.Response) => {
     logger.info('category...');
-    const type: any = req.query.type || undefined;
+    const type: CategoryTypeCode = req.query.type as any;
     logger.info('type:' + type);
 
-    const result = {} as ResponseType;
-    result.code = HttpStatus.OK;
-    result.message = HttpStatus["200"];
+    const result = new Response<Category[]>();
     const categoryService = new CategoryService();
     
     try {
-        const categories = await categoryService.getCategories(type);
+        const categories: Category[] = await categoryService.getCategories(type);
         result.data = categories;
     } catch (err) {
         console.log(err.message);
@@ -36,5 +34,63 @@ type Category = {
     routerName: string;
     type: string;
 }
+
+// type Category = {
+//     _id: string;
+//     name: string;
+//     routerName: string;
+//     type: string;
+// }
+
+// type ResponseConfig<T> = { code: HttpStatusCode, message: string; data: T; error?: any };
+
+// class Response<T> {
+//     code: HttpStatusCode = HttpStatusCode.OK;
+//     message: string;
+//     data: T;
+//     error: any;
+    
+//     Response(config:ResponseConfig<T>) {
+//         this.code = config.code;
+//         this.message = config.message;
+//         this.data = config.data;
+//         this.error = config.error;
+//     }
+// }
+
+// enum HttpStatusCode {
+//     OK = 200,
+//     NOT_FOUND = 404,
+// }
+
+
+
+// class ResponseBuilder<T> {
+//     private instance = new Response<T>();
+//     code(code: HttpStatusCode) {
+//         this.instance.code = code;
+//         return this;
+//     }
+//     message(message: string) {
+//         this.instance.message = message;
+//         return this;
+//     }
+//     data(data: T) {
+//         this.instance.data = data;
+//         return this;
+//     }
+//     error(error: any) {
+//         this.instance.error = error;
+//         return this;
+//     }
+//     build() {
+//         return this.instance;
+//     }
+// }
+
+
+
+
+
 
 export default router;
