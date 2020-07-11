17c3ac8dfb486e370fce6b80c6a0ba3675cd503f
limwoobin <drogba02@naver.com>
2020-07-11 16:48:33 +0900

    test commit

--- src/test.js
+++ src/test.js
@@ -1,16 +1,1 @@
-function Member(name) {
-    this.name = name;    
-}
-
-Member.staticMethod = function() { 
-    console.log(this.name); 
-}
-
-Member.prototype.getName = function() {
-     return this.name 
-};
-
-const mem = new Member("test");
-
-Member.staticMethod();
-console.log(mem.getName());
+console.log('111');
