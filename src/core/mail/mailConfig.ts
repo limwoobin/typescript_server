d84d0bae846ff6a4b926b7c7e2757513e1e80368
drogba <drogba02@naver.com>
2020-07-16 19:09:24 +0900

    mailConfig 생성

--- src/core/mail/mailConfig.ts
+++ src/core/mail/mailConfig.ts
@@ -4,3 +4,24 @@ const config = require('../config/config.json');
 import logger from '../config/winston';
 // import Member from '../../models/member';
 import crypto from 'crypto';
+import util from '../util/util';
+
+
+const transporter = nodemailer.createTransport(smtpTransport({
+    service: config.mailInfo.service,
+    host: config.mailInfo.host,
+    auth: {
+        user: config.mailInfo.mail,
+        pass: config.mailInfo.password,
+    }
+}));
+
+
+class mailConfig {
+
+    passwordFindMail(toEmail: string) {
+
+    }
+}
+
+export default mailConfig;
--- src/core/util/util.ts
+++ src/core/util/util.ts
@@ -2,8 +2,8 @@ import * as express from 'express';
 import { Request } from 'express-serve-static-core';
 import { Response , ResponseException} from '../response/ResponseType';
 import Board from '../../models/board';
-const common = require('../common/common');
-const Post = require('../models/post');
+import crypto from 'crypto';
+const Post = require('../../models/post');
 
 const result = new Response();
 
@@ -21,12 +21,11 @@ export default class util {
     }
 
     checkPostId(req: express.Request , res: express.Response , next: express.NextFunction) {
-        const result: any = common.result;
+        const result: any = new Response();
         const _id: string = req.params.id || req.body._id;
         Post.findOne({_id:_id} , (err:Error) => {
             if(err) {
                 result.code = 'DR02';
-                result.message = common.status.DR02;
                 result.data = err.message;
                 return res.json(result); 
             }
@@ -35,23 +34,29 @@ export default class util {
     }
 
     isLogged(req: express.Request , res: express.Response , next: express.NextFunction) {
-        const result: any = common.result;
+        const result: any = new Response();
         if(!req.user){
             result.code = 'DR03';
-            result.message = common.status.DR03;
             return res.json(result); 
         }
         next();
     }
 
     sessionCheck(req: SessionRequest , res: express.Response , next: express.NextFunction) {
-        const result: any = common.result;
+        const result: any = new Response();
         if(req.session.userEmail || req.session.key) {
             result.code = 'DR01';
-            result.message = common.status.DR01;
             return res.json(result); 
         }
         next();
+    }
+
+    async getRandomString() {
+        return new Promise(function(resolve , reject){
+            crypto.randomBytes(15, async (err, buf) => {
+                resolve(buf.toString('base64'));
+            });
+        });
     }
 }
 
