3205abe7dc279dc141975f8c8a4bbcc45f8c81ba
limwoobin <drogba02@naver.com>
2020-07-17 21:28:20 +0900

    파일 정리

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,29 +4,14 @@ import categoryRouter from './category/CategoryRoute';
 import boardRouter from './board/BoardRoute';
 import commentRouter from './comment/CommentRoute';
 import userRouter from './user/UserRoute';
-// const customerRouter = require('./customer/CustomerRoute');
 // const postRouter = require('./post/PostRoute');
-// const visitorRouter = require('./visitor/VisitorController');
 // const mailRouter = require('./mail/MailRoute');
-// const common = require('../common/common');
-
-// router.get('/search/:keyword' , (req: express.Request , res: express.Response) => {
-//     const result = common.result;
-//     result.code = 'DR00';
-//     result.message = common.status.DR00;
-    
-//     const keyword = req.params.keyword;
-//     result.data = keyword;
-//     return res.json(result);
-// })
 
 router.use('/category' , categoryRouter);
 router.use('/board' , boardRouter);
 router.use('/user' , userRouter);
 router.use('/comment' , commentRouter);
-// router.use('/customer' , customerRouter);
 // router.use('/post' , postRouter);
-// router.use('/visitor' , visitorRouter);
 // router.use('/mail' , mailRouter);
 
 export default router;
